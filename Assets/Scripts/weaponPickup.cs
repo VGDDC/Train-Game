00c2cff1bea9a182d99e11ffe2cd2d52a5807305
JamesOwens15 <78513378+JamesOwens15@users.noreply.github.com>
2022-11-22 10:41:50 -0500

    Weapon Pickup
    Implemented weapon pickup in other scene.

--- Assets/Scripts/weaponPickup.cs
+++ Assets/Scripts/weaponPickup.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class weaponPickup : MonoBehaviour
 {
     private Inventory weaponInventory;
-    private Inventory generalInventory;
+    //private Inventory generalInventory;
+    public GameObject itemButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        weaponInventory = GameObject.FindGameObjectWithTag("Weapon").GetComponent<Inventory>();
-        generalInventory = GameObject.FindGameObjectWithTag("Items").GetComponent<Inventory>();
+        weaponInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        //generalInventory = GameObject.FindGameObjectWithTag("Items").GetComponent<Inventory>();
     }
 
     //Pickup up a weapon and add it to your inventory when you collide with it.
     private void OnTriggerEnter2D(Collider2D pickup)
     {
-        if (pickup.CompareTag("Weapon"))
+        if (pickup.CompareTag("Player"))
         {
             for(int i=0; i<weaponInventory.slots.Length; i++)
             {
@@ -25,11 +26,12 @@ public class weaponPickup : MonoBehaviour
                 {
                     //Add weapon to weapon inventory
                     weaponInventory.isFull[i] = true;
+                    Instantiate(itemButton, weaponInventory.slots[i].transform, false);
                     Destroy(gameObject);
                 }
             }
         }
-        if(pickup.CompareTag("Inventory"))
+        /*if(pickup.CompareTag("Inventory"))
         {
             for(int i=0; i<generalInventory.slots.Length; i++)
             {
@@ -37,10 +39,11 @@ public class weaponPickup : MonoBehaviour
                 {
                     //Add item to inventory
                     generalInventory.isFull[i] = true;
+                    Instantiate(itemButton, generalInventory.slots[i].transform, false);
                     Destroy(gameObject);
                 }
             }
-        }
+        }*/
     }
 
     // Update is called once per frame
