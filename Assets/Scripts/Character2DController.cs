7a762e75b9c982d2beff0e43ff9a168654945419
vmathmachine <31744299+vmathmachine@users.noreply.github.com>
2023-03-30 13:35:09 -0400

    Minor Physics Update
    (Chris Maguschak) I froze the Z rotation of the cowboy and alien in the "Test Scene", and commented said cowboy's script.

--- Assets/Scripts/Character2DController.cs
+++ Assets/Scripts/Character2DController.cs
@@ -18,17 +18,19 @@ public class Character2DController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        //Here, we read the user's inputs to move the player in the desired direction
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
-        if (!Mathf.Approximately(0, movement))
-            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        if (!Mathf.Approximately(0, movement)) { //when we're not moving
+            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity; //make us face the correct direction
+        } //the if statement is there to stop us from always facing left any time we stop
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f) //if we press jump
         {
-            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse); //make us jump upwards
         }
     }
 
-    //Make a Dodge Function
+    //TODO Make a Dodge Function
 }
