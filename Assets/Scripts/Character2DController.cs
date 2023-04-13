cbf970c17a460cbde3eedf52a3c39227ff9ed46e
vmathmachine <31744299+vmathmachine@users.noreply.github.com>
2023-04-13 13:31:36 -0400

    Started the 3 way aiming code
    (Chris M) In the Character2DController script, I started writing the code that aims the bullet in one of 3 directions depending on our actions. We still have to add the code to it which instantiates bullets and gives them velocity. Also, once this is all up and running, we also have to remove the scripts that shoot multidirectionally. Also, possibly, when we're done, we might want to move the code for bullets to another script and make them public static methods.

--- Assets/Scripts/Character2DController.cs
+++ Assets/Scripts/Character2DController.cs
@@ -7,7 +7,10 @@ public class Character2DController : MonoBehaviour
     // Start is called before the first frame update
     public float MovementSpeed = 1;
     public float JumpForce = 1;
-
+    public bool aimUp = false;
+    public byte fireCoolDown = 0;
+    public bool facesRight = true; //TODO make it so this inits to true or false depending on how we start the level
+    
     private Rigidbody2D _rigidbody;
 
     private void Start()
@@ -18,19 +21,47 @@ public class Character2DController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        if(fireCoolDown != 0) { --fireCoolDown; } //update cooldown timer every frame
+        
+        ///////////////// MOVEMENTS /////////////////////////
+        
         //Here, we read the user's inputs to move the player in the desired direction
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
         if (!Mathf.Approximately(0, movement)) { //when we're not moving
             transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity; //make us face the correct direction
+            facesRight = movement > 0; //we're now facing right iff we're facing right
         } //the if statement is there to stop us from always facing left any time we stop
-
+        
+        //////////////// BUTTONS //////////////////////////
+        
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f) //if we press jump
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse); //make us jump upwards
         }
+        
+        if(Input.GetButtonDown("Aim Up")) { aimUp = true; } //enables upward aiming
+        if(Input.GetButtonUp("Aim Up")) { aimUp = false; } //disables upward aiming TODO make sure this works
+
+        if(Input.GetMouseButton(0) && fireCoolDown == 0) {
+            if(aimUp) {
+                //instantiate bullet above player, set velocity to be upwards
+            }
+            else if(facesRight) {
+                //instantiate bullet right of player, set velocity to be rightwards
+            }
+            else {
+                //instantiate bullet left of player, set velocity to be leftwards
+            }
+            //TODO actually instantiate the bullets & their velocities
+            
+            fireCoolDown = 40;
+        }
     }
 
     //TODO Make a Dodge Function
+    
+    //TODO lowercase the variables (please)
+    //example: movementSpeed instead of MovementSpeed
 }
